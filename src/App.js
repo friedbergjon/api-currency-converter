9b2c84746f18d5df31e3672a5f8c5c4b0d7ecda3
friedbergjon <friedberg_jon@hotmail.com>
2019-05-26 11:44:06 -0400

    created input handlers

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
     super(props);
 
     this.state = {
-     currencyFrom: "",
-      currencyTo: ""
+     from: "",
+     to: "",
+     amount: ""
+     
     }
 
     this.getCurrencyData = this.getCurrencyData.bind(this);
@@ -23,13 +25,13 @@ class App extends Component {
 
   async getCurrencyData(event) {
     event.preventDefault();
+   
     const url = "http://data.fixer.io/api/latest?access_key=";
     const api_key = "19a134dc8bb335545f8993b6da04b496";
     const res = await axios(`${url}${api_key}`);
-
     console.log(res.data);
     this.setState({
-  
+     
 //
   })
   }
@@ -40,11 +42,20 @@ class App extends Component {
     
     return (
       <div className="App">
+        <br />
        <form onSubmit={this.getCurrencyData}>
-          <input type="text" name="currency-from" value={this.state.currencyFrom} placeholder="currency-from" id="currency-from" onChange={this.handleInputChange}/>
-  	      <input type="submit" value="get-currency" id="getTemp"/>
-         
+      <div>
+          <input type="text" name="from" value={this.state.from} onChange={this.handleInputChange}/>
+          <h1>Country From</h1>
+          <input type="text" name="to" value={this.state.to} onChange={this.handleInputChange}/>
+          <h1>Country To</h1>
+          <input type="text" name="amount" value={this.state.amount} onChange={this.handleInputChange}/>
+          <h1>Amount</h1>
+  	      <input type="submit" value="Result"/>
+         <br />
+         </div>
         </form>
+       
       </div>
     )
   }
