1a56032502522072b17172d13c7091ad3083261c
friedbergjon <friedberg_jon@hotmail.com>
2019-05-28 11:04:35 -0400

    retrieving data from individual country based on input field

---
+++ src/Countries.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+
+
+
+// class Countries extends Component {
+
+//   render() {
+//     return (
+//         <div className="countries">
+//           <h1>{this.props.country}</h1>
+//         </div>
+
+      
+ 
+//     )
+//   }
+// }
+
+// export default Countries
