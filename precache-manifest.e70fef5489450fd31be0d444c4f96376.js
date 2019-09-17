659c4d8f54493e5fa991118182448214c647b6bf
friedbergjon <friedberg_jon@hotmail.com>
2019-09-16 22:35:22 -0400

    Updates

--- precache-manifest.e70fef5489450fd31be0d444c4f96376.js
+++ precache-manifest.e70fef5489450fd31be0d444c4f96376.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7ee8c87da92171401517bd94dfb5d4c",
+    "revision": "15b85bd22e78a4c84627bf498be5b6cd",
     "url": "/api-currency-converter/index.html"
   },
   {
-    "revision": "bae1128156bf9ba20984",
-    "url": "/api-currency-converter/static/css/main.ca26e5a1.chunk.css"
+    "revision": "5972646efe3850754f6d",
+    "url": "/api-currency-converter/static/css/main.aa4fa045.chunk.css"
   },
   {
     "revision": "b8233ab068e124168305",
     "url": "/api-currency-converter/static/js/2.db32b957.chunk.js"
   },
   {
-    "revision": "bae1128156bf9ba20984",
-    "url": "/api-currency-converter/static/js/main.2409e430.chunk.js"
+    "revision": "5972646efe3850754f6d",
+    "url": "/api-currency-converter/static/js/main.8f6ca4b5.chunk.js"
   },
   {
     "revision": "d9f44d55ff7c8c003dc5",
